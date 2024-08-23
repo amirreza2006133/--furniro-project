befa37089535b1630f6c2ecdd6e4986eb0bfa28e
Amirreza <amirmmm1385.58@email.com>
2024-08-24 04:03:56 +0430

    open cart aded

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
 const yearEL = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEL.textContent = currentYear;
+const Html = document.querySelector(".overflow");
+const CartOPenEL = document.querySelector(".main-btn-cart");
+const CloseEL = document.querySelector(".close-icon");
+
+CartOPenEL.addEventListener("click", function () {
+  CartOPenEL.classList.toggle("cart-open");
+  //   Html.style.overflow = "hidden";
+});
