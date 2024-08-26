a42016243cae331802b4c68bee433ed473255779
Amirreza <amirmmm1385.58@email.com>
2024-08-27 03:38:39 +0430

    product-filters aded

--- js/general.js
+++ js/general.js
@@ -1,6 +1,3 @@
-const yearEL = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-yearEL.textContent = currentYear;
 const Html = document.querySelector(".overflow");
 const CartOPenEL = document.querySelector(".main-btn-cart");
 const CloseEL = document.querySelector(".close-icon");
@@ -9,3 +6,6 @@ CartOPenEL.addEventListener("click", function () {
   CartOPenEL.classList.toggle("cart-open");
   //   Html.style.overflow = "hidden";
 });
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
