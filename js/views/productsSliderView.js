bdc84d27d534deb641357d134d2aafbd95decea8
mahdi <ramezani86mraz@gmail.com>
2024-09-27 00:27:46 +0330

    (feat) pagination for products slider implemented

--- js/views/productsSliderView.js
+++ js/views/productsSliderView.js
@@ -4,12 +4,16 @@ class ProductsSliderView {
   _swiper;
   _parentEl = document.querySelector(".products-slider-list");
   _navigationEl = document.querySelector(".products-slider-navigation");
+  _paginationEls = document.querySelectorAll(
+    ".products-slider-pagination>.dots"
+  );
 
   render(slides) {
     const generatedMarkup = this._generateMarkup(slides);
     this._parentEl.insertAdjacentHTML("beforeend", generatedMarkup);
     this._renderSwiper();
     this._renderNavigation();
+    this._renderPagination();
   }
 
   _renderSwiper() {
@@ -49,12 +53,35 @@ class ProductsSliderView {
   _renderNavigation() {
     this._navigationEl.addEventListener("click", (e) => {
       const target = e.target.closest("button");
+      if (!target) return;
       if (target.classList.contains("products-slider-prev-slide"))
         this._swiper.slidePrev();
-
       if (target.classList.contains("products-slider-next-slide"))
         this._swiper.slideNext();
+
+      this._handlePagination(this._swiper.realIndex);
     });
+  }
+
+  _renderPagination() {
+    const countSlides = this._swiper.slides.length;
+    let generatedMarkup = "";
+    for (let index = 0; index < countSlides; index++)
+      generatedMarkup += `<li></li>`;
+
+    this._paginationEls.forEach((el) => {
+      el.insertAdjacentHTML("beforeend", generatedMarkup);
+      el.children[0].classList.add("active");
+    });
+  }
+
+  _handlePagination(activeIndex) {
+    const alreadyActiveSlide = document.querySelector(".dots>li.active");
+    if (alreadyActiveSlide) alreadyActiveSlide.classList.remove("active");
+
+    this._paginationEls.forEach((el) =>
+      el.children[activeIndex].classList.add("active")
+    );
   }
 }
 
