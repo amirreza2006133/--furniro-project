71fdfaf97aabc4cc53c7d39bdf3d9c6cfe093b22
Amirreza <amirmmm1385.58@gmail.com>
2024-10-18 15:54:20 +0330

    (fix)Fixing style conflict of product cart buttons for different type of products view

--- js/views/productListView.js
+++ js/views/productListView.js
@@ -5,14 +5,14 @@ import compareIcon from "../../img/icons/compare-svgrepo-com 1.png";
 import { formatCurrency } from "../helper";
 
 class ProductListView {
-  _parentElement = document.querySelector(".product-section")
+  _parentElement = document.querySelector(".product-section");
   _boxParentElement = document.querySelector(".card-container");
   _inlineParentElement = document.querySelector(".card-inline-container");
 
   render(products, wishlist, viewMode = "box") {
     let generatedMarkup;
-    this._boxParentElement.innerHTML = ""
-    this._inlineParentElement.innerHTML = ""
+    this._boxParentElement.innerHTML = "";
+    this._inlineParentElement.innerHTML = "";
 
     if (viewMode === "box") {
       generatedMarkup = this._generateBoxMarkup(products);
@@ -21,7 +21,10 @@ class ProductListView {
 
     if (viewMode === "inline") {
       generatedMarkup = this._generateInlineMarkup(products);
-      this._inlineParentElement.insertAdjacentHTML("beforeend", generatedMarkup);
+      this._inlineParentElement.insertAdjacentHTML(
+        "beforeend",
+        generatedMarkup
+      );
     }
 
     this.renderWishlist(wishlist);
@@ -33,16 +36,20 @@ class ProductListView {
       this._parentElement.querySelectorAll(".pr-list-card");
 
     // resting icons
-    const currentLikedIcons = document.querySelectorAll(".wishlist-click-btn.liked");
-    currentLikedIcons.forEach(el => {
+    const currentLikedIcons = document.querySelectorAll(
+      ".wishlist-click-btn.liked"
+    );
+    currentLikedIcons.forEach((el) => {
       el.src = emptyHeartIcon;
       el.textContent = "like";
-      el.classList.remove("liked")
-    })
+      el.classList.remove("liked");
+    });
 
     productCardEls.forEach((card) => {
       const productId = Number(card.dataset.id);
-      const isProductInWishList = wishlist.some((product) => product.id === productId);
+      const isProductInWishList = wishlist.some(
+        (product) => product.id === productId
+      );
       if (!isProductInWishList) return;
 
       // if product is in user's wishlist, change it's icon and label
@@ -51,13 +58,19 @@ class ProductListView {
 
       if (isProductInWishList) {
         wishlistIcon.src = filledHeartIcon;
-        wishlistIcon.classList.add("liked")
-        wishlistLabel ? wishlistLabel.textContent = "unlike" : "";
+        wishlistIcon.classList.add("liked");
+        wishlistLabel ? (wishlistLabel.textContent = "unlike") : "";
       }
     });
   }
 
-  addEventHandler(renderProductList, renderWishlist, renderModal, wishlistClickHandler, goToDetailsPage) {
+  addEventHandler(
+    renderProductList,
+    renderWishlist,
+    renderModal,
+    wishlistClickHandler,
+    goToDetailsPage
+  ) {
     window.addEventListener("load", () => {
       renderProductList && renderProductList();
       renderWishlist && renderWishlist();
@@ -69,7 +82,8 @@ class ProductListView {
       const productId = e.target.closest(".pr-list-card")?.dataset.id;
       if (e.target.classList.contains("show-modal")) renderModal(productId);
       if (e.target.classList.contains("gradiant")) goToDetailsPage(productId);
-      if (e.target.classList.contains("wishlist-click-btn")) wishlistClickHandler(productId);
+      if (e.target.classList.contains("wishlist-click-btn"))
+        wishlistClickHandler(productId);
     });
   }
 
@@ -128,12 +142,18 @@ class ProductListView {
   }
 
   _generateInlineMarkup(products) {
-    return products.map(product => `
+    return products
+      .map(
+        (product) => `
       <div class="pr-list-card product-inline-card" data-id="${product.id}">
         <div class="product-inline-info">
         <div class="product-inline-img-box">
-          <div class="product-inline-tag ${product.new ? "inline-new" : ""}${product.discount ? "inline-off" : ""}">${product.discount || ""}${product.new ? "new" : ""}</div>
-          <img class="product-inline-img" src="${product.imageUrl}" alt=" a photo of a stylish caffe chair"/>
+          <div class="product-inline-tag ${product.new ? "inline-new" : ""}${
+          product.discount ? "inline-off" : ""
+        }">${product.discount || ""}${product.new ? "new" : ""}</div>
+          <img class="product-inline-img" src="${
+            product.imageUrl
+          }" alt=" a photo of a stylish caffe chair"/>
         </div>
           <div class="product-description">
             <h3 class="product-inline-heading">${product.name}</h3>
@@ -161,7 +181,7 @@ class ProductListView {
         </div>
 
         <div class="product-info-cta">
-              <a class="hover-btn show-modal" href="#"> Add to cart </a>
+              <a class="hover-btn btn-inline show-modal" href="#"> Add to cart </a>
               <div class="icons-inline-container">
                 <a href="#"><img src="${shareIcon}" alt="share" /></a>
                 <a href="#"><img src="${compareIcon}" alt="compare"/></a>
@@ -169,7 +189,9 @@ class ProductListView {
               </div>
         </div>
       </div>
-      `).join("")
+      `
+      )
+      .join("");
   }
 
   _calculateDiscountedPrice(product) {
